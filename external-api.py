90e6914bdefc4cb3c17ccc27a0f4785e8965c403
LilDjaga <100074655+LilDjaga@users.noreply.github.com>
2024-03-06 12:22:48 +0100

    added currentcondition support

--- external-api.py
+++ external-api.py
@@ -7,12 +7,15 @@ import json
 from datetime import datetime
 from dotenv import load_dotenv
 import os
-import DB
+# import DB
+
+API_temp = "https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/Apeldoorn/next7days?unitGroup=metric&elements=datetime%2Ctemp%2Cwinddir%2Cpressure&include=hours%2Ccurrent&key=ZW8NCV6JP8ZUGX33D769DJ693&contentType=json"
 
 load_dotenv()
 
 app = FastAPI()
 
+# response = requests.request("GET", API_temp)
 response = requests.request("GET", os.getenv("API_KEY"))
 if response.status_code != 200:
     print("Unexpected Status code: ", response.status_code)
@@ -21,6 +24,19 @@ if response.status_code != 200:
 # Parse the results as JSON
 jsonData = response.json()
 address = jsonData["address"]
+for currentCondition_data, value in jsonData["currentConditions"].items():
+    match currentCondition_data:
+        case "datetime":
+            currentCondition_time = value
+        case "temp":
+            currentCondition_temp = value
+        case "winddir":
+            currentCondition_winddir = value
+        case "pressure":
+            currentCondition_pressure = value
+            print(
+                                    f"{currentCondition_time} values: temp: {currentCondition_temp}, winddir: {currentCondition_winddir}, pressure: {currentCondition_pressure}"
+                                )
 
 for day in jsonData["days"]:
     date_day = datetime.strptime(day["datetime"], "%Y-%m-%d").date()
@@ -28,8 +44,6 @@ for day in jsonData["days"]:
         match day_data:
             case "temp":
                 day_temp = value
-            case "humidity":
-                day_humidity = value
             case "winddir":
                 day_winddir = value
             case "pressure":
@@ -44,13 +58,11 @@ for day in jsonData["days"]:
                                 date_day_hour = datetime.combine(date_day, date_hour)
                             case "temp":
                                 hour_temp = value
-                            case "humidity":
-                                hour_humidity = value
                             case "winddir":
                                 hour_winddir = value
                             case "pressure":
                                 hour_pressure = value
                                 print(
-                                    f"{date_day_hour} values: temp: {hour_temp}, humidity: {hour_humidity}, winddir: {hour_winddir}, pressure: {hour_pressure}"
+                                    f"{date_day_hour} values: temp: {hour_temp}, winddir: {hour_winddir}, pressure: {hour_pressure}"
                                 )
 
