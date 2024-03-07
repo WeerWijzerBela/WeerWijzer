0d0994433d9bb3e6847185ae6beddd17aed866d8
Olaf Majoor <olafmajoor@hotmail.com>
2024-03-07 11:17:53 +0100

    test to insert data

---
+++ test.py
@@ -0,0 +1,29 @@
+import DB
+
+def insert_data(cursor):
+    """
+    Inserts data into the database.
+    """
+    insert_query = '''
+    INSERT INTO Locaties (locatie) VALUES ('Apeldoorn');
+    '''
+    try:
+        cursor.execute(insert_query)
+        print("Data inserted successfully.")
+    except DB.database.Error as e:
+        print(f"Error inserting data: {e}")
+
+
+
+def main():
+    connection = DB.connect_to_database()
+    if connection:
+        try:
+            cursor = connection.cursor()
+            insert_data(cursor)
+        finally:
+            cursor.close()
+            connection.close()
+
+if __name__ == "__main__":
+    main()
