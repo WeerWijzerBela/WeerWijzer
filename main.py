cc65d330527957ed7255dabab4f8959e771ee8bc
= <justin.punt@novi-education.nl>
2024-03-12 15:54:53 +0100

    fixed de main.py

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ print(int(datetime.strptime("16:00:00", "%H:%M:%S").strftime("%H")))
 
 for i in range(10):
     for i in range(5):
-    if datetime.now().hour >= int(datetime.strptime("16:00:00", "%H:%M:%S").strftime("%H")):
-        print(i)
-        continue
+        if datetime.now().hour >= int(datetime.strptime("16:00:00", "%H:%M:%S").strftime("%H")):
+            print(i)
+            continue
     print("test")
