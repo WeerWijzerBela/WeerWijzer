0d16d2f1839ce1cedc04f23800b7aaa634536134
Olaf Majoor <olafmajoor@hotmail.com>
2024-03-19 16:20:04 +0100

    FLAKE 8

--- metingen.py
+++ metingen.py
@@ -1,13 +1,16 @@
 import requests
 import sys
-from datetime import datetime,date
+from datetime import datetime, date
 import DB
 
 
 locatie = "Apeldoorn"
-API_temp = f"https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/{locatie}\
+API_temp = f"https://weather.visualcrossing.com/\
+    VisualCrossingWebServices/rest/services/timeline/{locatie}\
     /next7days?unitGroup=metric&elements=datetime%2Ctemp%2Cwinddir%2Cpressure&\
     include=hours%2Ccurrent&key=ZW8NCV6JP8ZUGX33D769DJ693&contentType=json"
+
+
 def post_external_data():
     """
     posts data from an external API.
@@ -17,26 +20,33 @@ def post_external_data():
         jsonData = response.json()
         connection = DB.connect_to_database()
         cursor = connection.cursor()
-        insertMetingen = f"INSERT INTO metingen (locatieId, datetime) VALUES ((SELECT locatieId FROM locaties WHERE locatie = \
+        insertMetingen = f"INSERT INTO metingen (locatieId, datetime) \
+            VALUES ((SELECT locatieId FROM locaties WHERE locatie = \
             '{locatie}'), '{datetime.now().strftime('%Y-%m-%d %H:%M:%S')}')"
         cursor.execute(insertMetingen)
         connection.commit()
         metingId = cursor.lastrowid
-        insertQuery = f"INSERT INTO metinguren (metingenId,datetime,temperature,pressure,winddirection)VALUES \
-              ('{metingId}', '{str(date.today())  + ' ' + jsonData['currentConditions']['datetime']}', \
-                '{jsonData['currentConditions']['temp']}', '{jsonData['currentConditions']['pressure']}', \
-                    '{jsonData['currentConditions']['winddir']}')"
+        insertQuery = f"INSERT INTO metinguren \
+            (metingenId,datetime,temperature,pressure,winddirection)VALUES \
+            ('{metingId}', '{str(date.today())  + ' \
+            ' + jsonData['currentConditions']['datetime']}', \
+            '{jsonData['currentConditions']['temp']}',\
+            '{jsonData['currentConditions']['pressure']}', \
+            '{jsonData['currentConditions']['winddir']}')"
         cursor.execute(insertQuery)
         connection.commit()
 
         for data in jsonData["days"]:
             for i in data['hours']:
                 i['datetime'] = data['datetime'] + ' ' + i['datetime']
-                if i['datetime'] <= str(date.today())  + ' ' + jsonData['currentConditions']['datetime']:
+                if i['datetime'] <= str(date.today())  + ' \
+                      ' + jsonData['currentConditions']['datetime']:
                     continue
                 else:
-                    insertMetingUren = f"INSERT INTO metinguren(metingenId,datetime,temperature,pressure,winddirection) \
-                        VALUES ('{metingId}', '{i['datetime']}', {i['temp']}, {i['pressure']}, '{i['winddir']}')"
+                    insertMetingUren = f"INSERT INTO metinguren\
+                        (metingenId,datetime,temperature,pressure,winddirection) \
+                        VALUES ('{metingId}', '{i['datetime']}',\
+                        {i['temp']}, {i['pressure']}, '{i['winddir']}')"
                     cursor.execute(insertMetingUren)
                     connection.commit()
     else:
