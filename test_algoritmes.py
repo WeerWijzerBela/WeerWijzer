3df5852802cb9c379fba9b68fc2a4f9fc1db88a2
= <justin.punt@novi-education.nl>
2024-03-20 14:04:39 +0100

    test zonder pytest

--- test_algoritmes.py
+++ test_algoritmes.py
@@ -1,14 +1,22 @@
-import pytest
-from runWeerWijzer import *
+# import pytest
+# from runWeerWijzer import *
+#
+#
+# class TestWeerberekenen:
+#     def test_zambretti(self):
+#         assert bereken_zambretti(1000, 990, 180) == 27
+#
+#     def test_zambretti_onvoorspelbaar(self):
+#         assert bereken_zambretti(945, 940, 180) == 5
+#
+#
+# if __name__ == "__main__":
+#     pytest.main()
 
+def add_numbers(a, b):
+    return a + b
 
-class TestWeerberekenen:
-    def test_zambretti(self):
-        assert bereken_zambretti(1000, 990, 180) == 27
-
-    def test_zambretti_onvoorspelbaar(self):
-        assert bereken_zambretti(945, 940, 180) == 5
-
-
-if __name__ == "__main__":
-    pytest.main()
+def test_add_numbers():
+    assert add_numbers(1, 2) == 3
+    assert add_numbers(-1, 1) == 0
+    assert add_numbers(0, 0) == 0
