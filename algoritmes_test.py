ac490d9d965bfccb6c2834d5588b5c92a446e6c1
= <justin.punt@novi-education.nl>
2024-03-20 13:36:52 +0100

    kijken of de pytest iets doet

---
+++ algoritmes_test.py
@@ -0,0 +1,14 @@
+import unittest
+from runWeerWijzer import *
+
+
+class TestWeerberekenen(unittest.TestCase):
+    def test_zambretti(self):
+        self.assertEqual(bereken_zambretti(1000, 990, 180), 27)
+
+    def test_zambretti_onvoorspelbaar(self):
+        self.assertEqual(bereken_zambretti(945, 940, 180), 999)
+
+
+if __name__ == "__main__":
+    unittest.main()
