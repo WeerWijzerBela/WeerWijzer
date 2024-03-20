82d9cdbf5ea51cccac4a44265efd8e1356a5e5e7
= <justin.punt@novi-education.nl>
2024-03-20 13:48:14 +0100

    nieuwe test 2

--- algoritmes_test.py
+++ algoritmes_test.py
@@ -2,5 +2,5 @@ def add_numbers(a, b):
     return a + b
 def test_add_numbers():
     assert add_numbers(1, 2) == 3
-    assert add_numbers(-1, 1) == 1
+    assert add_numbers(-1, 1) == 0
     assert add_numbers(0, 0) == 0
