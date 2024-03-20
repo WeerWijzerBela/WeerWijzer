fde4e49a2c3a01fe0f9f8067d9eb320173b20f8a
= <justin.punt@novi-education.nl>
2024-03-20 14:18:48 +0100

    verwijder duplicaat

--- tests/test_algoritmes.py
+++ tests/test_algoritmes.py
@@ -1,23 +1,15 @@
-# import pytest
-# from runWeerWijzer import *
-#
-#
-# class TestWeerberekenen:
-#     def test_zambretti(self):
-#         assert bereken_zambretti(1000, 990, 180) == 27
-#
-#     def test_zambretti_onvoorspelbaar(self):
-#         assert bereken_zambretti(945, 940, 180) == 5
-#
+import pytest
+from runWeerWijzer import *
 
-#
-# if __name__ == "__main__":
-#     pytest.main()
 
-def add_numbers(a, b):
-    return a + b
+class TestWeerberekenen:
+    def test_zambretti(self):
+        assert bereken_zambretti(1000, 990, 180) == 27
 
-def test_add_numbers():
-    assert add_numbers(1, 2) == 3
-    assert add_numbers(-1, 1) == 0
-    assert add_numbers(0, 0) == 0
+    def test_zambretti_onvoorspelbaar(self):
+        assert bereken_zambretti(945, 940, 180) == 5
+
+
+
+if __name__ == "__main__":
+    pytest.main()
