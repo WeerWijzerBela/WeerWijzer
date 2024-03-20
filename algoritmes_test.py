526442b7c06e9e4c5ebdda4649d58e362cb88f9f
= <justin.punt@novi-education.nl>
2024-03-20 13:47:11 +0100

    nieuwe test

--- algoritmes_test.py
+++ algoritmes_test.py
@@ -1,14 +1,6 @@
-import unittest
-from runWeerWijzer import *
-
-
-class TestWeerberekenen(unittest.TestCase):
-    def test_zambretti(self):
-        self.assertEqual(bereken_zambretti(1000, 990, 180), 27)
-
-    def test_zambretti_onvoorspelbaar(self):
-        self.assertEqual(bereken_zambretti(945, 940, 180), 999)
-
-
-if __name__ == "__main__":
-    unittest.main()
+def add_numbers(a, b):
+    return a + b
+def test_add_numbers():
+    assert add_numbers(1, 2) == 3
+    assert add_numbers(-1, 1) == 1
+    assert add_numbers(0, 0) == 0
