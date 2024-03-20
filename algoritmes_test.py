3e12172c5c9464308e29a7005b9595e397329cff
Olaf Majoor <olafmajoor@hotmail.com>
2024-03-20 13:53:11 +0100

    check if error

--- algoritmes_test.py
+++ algoritmes_test.py
@@ -1,6 +1,14 @@
-def add_numbers(a, b):
-    return a + b
-def test_add_numbers():
-    assert add_numbers(1, 2) == 3
-    assert add_numbers(-1, 1) == 0
-    assert add_numbers(0, 0) == 0
+import unittest
+from runWeerWijzer import *
+
+
+class TestWeerberekenen(unittest.TestCase):
+    def test_zambretti(self):
+        self.assertEqual(bereken_zambretti(1000, 990, 180), 27)
+
+    def test_zambretti_onvoorspelbaar(self):
+        self.assertEqual(bereken_zambretti(945, 940, 180), 5)
+
+
+if __name__ == "__main__":
+    unittest.main()
