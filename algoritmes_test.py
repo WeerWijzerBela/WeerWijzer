4fa8ac743d088169496bf22b13b2b08e38f07142
= <justin.punt@novi-education.nl>
2024-03-20 14:28:13 +0100

    weer in home directory

---
+++ algoritmes_test.py
@@ -0,0 +1,15 @@
+import pytest
+from runWeerWijzer import *
+
+
+class TestWeerberekenen:
+    def test_zambretti(self):
+        assert bereken_zambretti(1000, 990, 180) == 27
+
+    def test_zambretti_onvoorspelbaar(self):
+        assert bereken_zambretti(945, 940, 180) == 5
+
+
+
+if __name__ == "__main__":
+    pytest.main()
