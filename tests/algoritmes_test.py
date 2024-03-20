9e133e203944ba5f56a485500c2f3ceb42b12e3d
= <justin.punt@novi-education.nl>
2024-03-20 15:12:03 +0100

    push to olaf

--- tests/algoritmes_test.py
+++ tests/algoritmes_test.py
@@ -5,7 +5,9 @@ import pytest
 # import sys
 # parent_path = Path(__file__).resolve().parents[1]
 # sys.path.append(str(parent_path))
-from runWeerWijzer import bereken_zambretti
+# from runWeerWijzer import bereken_zambretti
+from ..runWeerWijzer import *
+
 
 
 class TestWeerberekenen:
