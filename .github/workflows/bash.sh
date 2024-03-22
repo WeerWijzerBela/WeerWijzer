c1d760d4a20cbeccc425ebdc8fe8c3db70caf129
= <justin.punt@novi-education.nl>
2024-03-22 09:21:42 +0100

    test met bash.sh

---
+++ .github/workflows/bash.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+active_gc_tasks=$(doctl registry garbage-collection list --format ID,Status --no-header | grep -c "Active")
+if [ "$active_gc_tasks" -eq 0 ]; then
+  echo "Er zijn geen actieve garbage collection-taken. Start de garbage collection."
+  run_gc=true
+else
+  echo "Er zijn nog actieve garbage collection-taken. Wacht tot deze zijn voltooid voordat je een nieuwe taak start."
+  run_gc=false
+fi
