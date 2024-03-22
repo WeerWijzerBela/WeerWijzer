8886ea165474c4645bf8bee0a12940d341eacee3
= <justin.punt@novi-education.nl>
2024-03-22 10:11:22 +0100

    bash en stop aangepast

--- .github/workflows/stop.sh
+++ .github/workflows/stop.sh
@@ -3,12 +3,6 @@
 active_gc_tasks=$(doctl registry garbage-collection list --format ID,Status --no-header | grep -c "Active")
 if [ "$active_gc_tasks" -eq 0 ]; then
   echo "Er zijn geen actieve garbage collection-taken. "
-  run_gc=true
 else
-  echo "Er zijn nog actieve garbage collection-taken. "
-  run_gc=false
-fi
-
-if [ "$run_gc" = "false" ]; then
-    doctl registry garbage-collection start --cancel-active
+  doctl registry garbage-collection cancel
 fi
