53ea7ad3f2517092fc813bb4391fa9bd8edadd6a
= <justin.punt@novi-education.nl>
2024-04-05 14:27:38 +0200

    bijwerken

---
+++ iac/container.tf
@@ -0,0 +1,6 @@
+## Create a new container registry
+#resource "digitalocean_container_registry" "container-do" {
+#  name                   = "weerwijzer-container"
+#  subscription_tier_slug = "starter"
+#  region                 = "ams3"
+#}
