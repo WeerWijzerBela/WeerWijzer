670f633d7b54cd8485ebf19ec051716ebd8a766e
= <justin.punt@novi-education.nl>
2024-04-05 10:56:33 +0200

    push push

--- iac/output.tf
+++ iac/output.tf
@@ -1,4 +1,3 @@
 output "weerwijzer_app_service_ip" {
-  value = kubernetes_service.weerwijzer_app_service.load_balancer_ip
-
+  value = kubernetes_service.weerwijzer_app_service.status.0.load_balancer.0.ingress.0.ip
 }
