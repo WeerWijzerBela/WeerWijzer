cbb8a76b23f35fbc518e2c9820744f41ddebe8ab
Olaf Majoor <olafmajoor@hotmail.com>
2024-04-08 16:05:40 +0200

    op manifest dan maar

--- .github/workflows/bash.sh
+++ .github/workflows/bash.sh
@@ -4,12 +4,12 @@ for repo in $repositories; do
     echo "Processing repository: $repo"
 
     # List and sort tags for the repository, keeping the latest 2
-    tags_to_delete=$(doctl registry repository list-tags weerwijzer-app -o json | jq -r '.[] | .tag ' | sort -r | tail -n +3)
+    tags_to_delete=$(doctl registry repository list-tags weerwijzer-app -o json | jq -r '.[] | .manifest_digest ' | sort -r | tail -n +3)
 
     echo "Tags to delete: $tags_to_delete"
 
     for tag in $tags_to_delete; do
-        echo "Deleting tag $tag from $repo"
+        echo "Deleting manifest $tag from $repo"
         # Delete the tag
         doctl registry repository delete-manifest $repo $tag --force
     done
