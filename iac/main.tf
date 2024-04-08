15607672431a0c8a994e87b3bb608ce91a0879c6
= <justin.punt@novi-education.nl>
2024-04-08 19:54:25 +0200

    grafana dashboard test

--- iac/main.tf
+++ iac/main.tf
@@ -125,4 +125,15 @@ resource "kubernetes_service" "weerwijzer_app_service" {
 }
 
 
+resource "grafana_folder" "test" {
+  title = "My Folder"
+  uid   = "my-folder-uid"
+}
 
+resource "grafana_dashboard" "test" {
+  folder = grafana_folder.test.uid
+  config_json = jsonencode({
+    "title" : "My Dashboard",
+    "uid" : "my-dashboard-uid"
+  })
+}
