ccb39c6e07c627a32972152cd77908b92585c433
Olaf Majoor <olafmajoor@hotmail.com>
2024-04-08 17:42:50 +0200

    dit is de fix

--- .github/workflows/bash.sh
+++ .github/workflows/bash.sh
@@ -6,7 +6,7 @@ for repo in $repositories; do
    echo "Processing repository: $repo"
 
    # List and sort tags for the repository, keeping the latest 2
-   tags_to_delete=$(doctl registry repository list-tags $repo -o json | jq -r '.[] | .tag +" " + .digest' | sort -r | tail -n +3 )
+   tags_to_delete=$(doctl registry repository list-tags $repo -o json | jq -r '.[] | .tag +" " + .manifest_digest' | sort -r | tail -n +3 )
 
    echo "$tags_to_delete" | while read line; do
        tag=$(echo "$line" | cut -d ' ' -f1)
