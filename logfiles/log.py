33bbfe8e04c35b7b39eeae1e3756719f3d8aeee3
= <justin.punt@novi-education.nl>
2024-04-09 16:04:47 +0200

    logging test 3

--- logfiles/log.py
+++ logfiles/log.py
@@ -8,15 +8,15 @@ logging.basicConfig(
     level=logging.DEBUG,
     format="%(asctime)s - %(levelname)s - %(message)s",
 )
-# console_handler = logging.StreamHandler()
-# console_handler.setLevel(logging.DEBUG)
+console_handler = logging.StreamHandler()
+console_handler.setLevel(logging.DEBUG)
 # formatter = logging.Formatter("%(asctime)s - %(levelname)s - %(message)s")
 # console_handler.setFormatter(formatter)
-# logging.getLogger().addHandler(console_handler)
+logging.getLogger().addHandler(console_handler)
 
 # Logtail configuratie
 handler = LogtailHandler(source_token="26NkjQezsymTKrzueVAB9aV8")
 logging = logging.getLogger(__name__)
-logging.setLevel(logging.INFO)
-logging.handlers = []
-logging.addHandler(handler)
+#logging.setLevel(logging.DEBUG)
+#logging.handlers = []
+logging.addHandler(console_handler)
