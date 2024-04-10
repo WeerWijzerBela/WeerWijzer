5a1e8dec9df57c0c5a88329735ad22fa0fc15d3d
= <justin.punt@novi-education.nl>
2024-04-10 15:52:55 +0200

    justins test

--- iac-container/database.tf
+++ iac-container/database.tf
@@ -22,7 +22,7 @@ data "github_repository" "repo" {
 resource "github_actions_organization_secret" "database_user_2" {
   depends_on = [data.github_repository.repo]
   secret_name             = "DB_USER_2"
-  visibility              = "private"
+  visibility              = "all"
   plaintext_value         = digitalocean_database_cluster.db-cluster.user
   # selected_repository_ids = [data.github_repository.repo.repo_id]
 }
@@ -30,7 +30,20 @@ resource "github_actions_organization_secret" "database_user_2" {
 resource "github_actions_organization_secret" "database_password_2" {
   depends_on = [data.github_repository.repo]
   secret_name             = "DB_PASSWORD_2"
-  visibility              = "private"
+  visibility              = "all"
   plaintext_value         = digitalocean_database_cluster.db-cluster.password
   # selected_repository_ids = [data.github_repository.repo.repo_id]
+}
+resource "github_repository_environment" "repo_environment" {
+  depends_on = [data.github_repository.repo]
+  repository       = data.github_repository.repo.name
+  environment      = "example_environment"
+}
+
+resource "github_actions_environment_secret" "test_secret" {
+  depends_on = [data.github_repository.repo]
+  repository       = data.github_repository.repo.name
+  environment      = github_repository_environment.repo_environment.environment
+  secret_name      = "test_secret_name"
+  plaintext_value  = "%s"
 }
