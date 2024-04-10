5910b6c3dc677fac62589ea38b3bbd3a3facfc6f
= <justin.punt@novi-education.nl>
2024-04-10 11:06:57 +0200

    nu organisation secret ipv actions

--- iac-container/database.tf
+++ iac-container/database.tf
@@ -14,21 +14,23 @@ resource "digitalocean_database_db" "database-mysql" {
 }
 
 
-#data "github_repository" "repo" {
-#  full_name = "WeerWijzerBela/WeerWijzer"
-#}
-resource "github_actions_secret" "database_user_2" {
+data "github_repository" "repo" {
   depends_on = [digitalocean_database_cluster.db-cluster]
-  repository       = "WeerWijzerBela/WeerWijzer"
-  secret_name      = "DB_USER_2"
-  plaintext_value  = digitalocean_database_cluster.db-cluster.user
+  full_name = "WeerWijzerBela/WeerWijzer"
 }
 
-resource "github_actions_secret" "database_password_2" {
+resource "github_actions_organization_secret" "database_user_2" {
   depends_on = [digitalocean_database_cluster.db-cluster]
-  repository       = "example_repository"
-  secret_name      = "DB_PASSWORD_2"
-  encrypted_value  = digitalocean_database_cluster.db-cluster.password
+  secret_name             = "DB_USER_2"
+  visibility              = "selected"
+  plaintext_value         = digitalocean_database_cluster.db-cluster.user
+  selected_repository_ids = [data.github_repository.repo.repo_id]
 }
 
-
+resource "github_actions_organization_secret" "database_password_2" {
+  depends_on = [digitalocean_database_cluster.db-cluster]
+  secret_name             = "DB_PASSWORD_2"
+  visibility              = "selected"
+  encrypted_value         = digitalocean_database_cluster.db-cluster.password
+  selected_repository_ids = [data.github_repository.repo.repo_id]
+}
